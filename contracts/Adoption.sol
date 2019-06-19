fd03ec0b314c5dcedc523ce7df0fe2733e60fca8
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-06-18 17:09:20 -0700

    added function to adopt a pet

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,5 +1,13 @@
 pragma solidity ^0.5.0;
 
 contract Adoption {
-    
+    address[16] public adopters;
+    //  Adopting apet
+    function adopt(uint petId) public returns (uint) {
+        require(petId >= && petId <= 15);
+
+        adopters[petId] = msg.sender;
+
+        return petId;
+    }
 }
