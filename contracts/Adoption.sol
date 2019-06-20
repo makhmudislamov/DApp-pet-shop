90af04e996e9297e057b052110bc8f9a33e1a622
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-06-20 16:10:19 -0700

    added getAdopters() func

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -4,10 +4,15 @@ contract Adoption {
     address[16] public adopters;
     //  Adopting apet
     function adopt(uint petId) public returns (uint) {
-        require(petId >= && petId <= 15);
+        require(petId >= 0 && petId <= 15);
 
         adopters[petId] = msg.sender;
 
         return petId;
+    }
+
+    // Retrieveing the adopters
+    function getAdopters() public view returns (adress[16] memory) {
+        return adopters;
     }
 }
